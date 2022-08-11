8a98be0d09c5fce3cb79605f5c4919c9f74eaeb4
Thiago Reis Nascimento <trnasci@gmail.com>
2022-08-11 15:47:53 -0300

    correção de lint do requisito 4

--- script.js
+++ script.js
@@ -26,9 +26,8 @@ const createProductItemElement = ({ sku, name, image }) => {
 
 const getSkuFromProductItem = (item) => item.querySelector('span.item__sku').innerText;
 
-const cartItemClickListener = () => {
-  const liCart = document.querySelector('.cart__items');
-  console.log(liCart);
+const cartItemClickListener = (event) => {
+  // coloque seu código aqui
 };
 
 const createCartItemElement = ({ sku, name, salePrice }) => {
